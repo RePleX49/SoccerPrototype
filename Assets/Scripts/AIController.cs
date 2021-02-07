7140a5a74872d89fcde997d7e32a6a070baed2bb
Luke Lin <lukelin2009@hotmail.com>
2021-02-06 16:11:44 -0800

    Changed AIController to use Rigidbody Movement

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class AIController : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed = 10.0f;
 
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,8 +24,12 @@ public class AIController : MonoBehaviour
             return;
         }
 
-        transform.LookAt(ballTransform);
+        //find the ball direction and set forward
+        Vector3 ballDirection = ballTransform.position - transform.position;
+        ballDirection.y = 0.0f;
+        transform.forward = ballDirection;
 
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        Vector3 newPosition = rb.position + (transform.forward * moveSpeed * Time.deltaTime);
+        rb.MovePosition(newPosition);
     }
 }
